cef74835134d9408b88390f16687d42cd1f5b86a
Alexander <145605030+alexgol296@users.noreply.github.com>
2024-03-07 18:46:21 -0700

    first program

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("Hello World")
-print("Hello Git")
+from math import sin, cos, sqrt, atan2, radians
+
+# Approximate radius of earth in km
+RADIUS = 6371.01
+R = 6371.01
+
+lat1 = radians(50.45)
+lon1 = radians(30.523)
+lat2 = radians(51.5072)
+lon2 = radians(-0.1275)
+
+dlon = lon2 - lon1
+dlat = lat2 - lat1
+
+a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+distance = 6371.01 * c
+print(distance)
